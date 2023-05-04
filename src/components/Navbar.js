2cf26be89e9afa0634eb83e599f513f64594dd14
HChanWoo <cksdn1st@naver.com>
2023-05-04 22:03:26 +0900

    create Navbar frame

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import "../styles/color.css";
+import "../styles/font.css";
+
+export default function Navbar() {
+    return(
+        <div style={{width:'222px',height:'100vh', backgroundColor:'black', 
+        borderRight:'3px solid var(--main-color)', position:'fixed'}}>
+        </div>
+    );
+}
