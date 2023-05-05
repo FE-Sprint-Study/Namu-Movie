5968b34918ee2ca5c708800a8c1996d9377953d2
HChanWoo <cksdn1st@naver.com>
2023-05-05 13:55:19 +0900

    fix: Prettier Eslint setting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-import "../styles/color.css";
-import "../styles/font.css";
+import '../styles/color.css';
+import '../styles/font.cs';
 
 export default function Navbar() {
-    return(
-        <div style={{width:'222px',height:'100vh', backgroundColor:'black', 
-        borderRight:'3px solid var(--main-color)', position:'fixed'}}>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        width: '222px',
+        height: '100vh',
+        backgroundColor: 'black',
+        borderRight: '3px solid var(--main-color)',
+        position: 'fixed',
+      }}
+    ></div>
+  );
 }
