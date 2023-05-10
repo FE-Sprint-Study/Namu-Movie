f7a66567ef0d2cbc05d0c4b93063ae286e6676e1
crowcrow07 <skathd3402@gmail.com>
2023-05-10 14:00:21 +0900

    feat : 라우팅 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import tw from 'tailwind-styled-components';
 import Navbar from './components/Navbar';
 import Main from './pages/Main';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
       <AppWrapper>
         <Navbar curPage="Home" />
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/genre" element={<Main />} />
+        </Routes>
       </AppWrapper>
     </div>
   );
