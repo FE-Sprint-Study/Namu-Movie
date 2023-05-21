cefc7958376eff27a727e061d1f8369e60ff9aa9
crowcrow07 <skathd3402@gmail.com>
2023-05-21 23:07:05 +0900

    feat : 메인 swiper 배치 및 swiper 시간 수정, 테일윈드 커스터마이징 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components';
 
+import Swiper from '../components/Swiper';
 import MovieCard from '../components/MovieCard';
 import movieApi from '../apis/movieApi';
 
 export default function Main() {
   const [movieData, setMovieData] = useState(null);
+
   useEffect(() => {
     movieApi
       .getPopular()
@@ -17,10 +19,53 @@ export default function Main() {
 
   return (
     <MainContainer>
-      {movieData &&
-        movieData.results.map(data => {
-          return <MovieCard movieData={data} key={movieData.id} />;
-        })}
+      <div>
+        <div className="flex justify-center items-center">
+          {movieData && <Swiper movieData={movieData} />}
+        </div>
+        <div className="flex flex-col text-white m-8 ml-20 mr-20">
+          <div className="text-5xl font-bold">최신 인기 영화</div>
+
+          <div className="flex flex-row w-full overflow-x-scroll">
+            {movieData &&
+              movieData.results.map(data => {
+                return <MovieCard movieData={data} key={movieData.id} />;
+              })}
+          </div>
+          <div className="text-5xl font-bold">최신 인기 영화</div>
+
+          <div className="flex flex-row w-full overflow-x-scroll">
+            {movieData &&
+              movieData.results.map(data => {
+                return <MovieCard movieData={data} key={movieData.id} />;
+              })}
+          </div>
+          <div className="text-5xl font-bold">최신 인기 영화</div>
+
+          <div className="flex flex-row w-full overflow-x-scroll">
+            {movieData &&
+              movieData.results.map(data => {
+                return <MovieCard movieData={data} key={movieData.id} />;
+              })}
+          </div>
+          <div className="text-5xl font-bold">최신 인기 영화</div>
+
+          <div className="flex flex-row w-full overflow-x-scroll">
+            {movieData &&
+              movieData.results.map(data => {
+                return <MovieCard movieData={data} key={movieData.id} />;
+              })}
+          </div>
+          <div className="text-5xl font-bold">최신 인기 영화</div>
+
+          <div className="flex flex-row w-full overflow-x-scroll">
+            {movieData &&
+              movieData.results.map(data => {
+                return <MovieCard movieData={data} key={movieData.id} />;
+              })}
+          </div>
+        </div>
+      </div>
     </MainContainer>
   );
 }
@@ -29,6 +74,9 @@ const MainContainer = tw.div`
   w-[calc(100%-14rem)]
   ml-auto
   flex
-  flex-wrap
+  flex-col
+  relative
+  right-0
+  overflow-scroll
   h-screen
 `;
