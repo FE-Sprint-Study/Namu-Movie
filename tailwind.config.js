c2a1e8a594e71b6f7d96afd5553ae228f6f26112
crowcrow07 <skathd3402@gmail.com>
2023-05-21 23:04:56 +0900

    feat : 테일윈드 커스터마이징 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ module.exports = {
         searchResultContainer: '77rem',
         searchBarWidth: '30rem',
       },
+      height: {
+        '50rem': '50rem',
+      },
+      dropShadow: {
+        '3xl': [
+          '0 10px 8px rgba(0, 0, 0, 0.3)',
+          '0 4px 3px rgba(0, 0, 0, 0.3)',
+        ],
+      },
+      minWidth: {
+        60: '15rem',
+      },
     },
   },
   plugins: [],
