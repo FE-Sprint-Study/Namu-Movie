90aeb7234cd9ce934ec6e0b85cf43ead2f32230f
HChanWoo <cksdn1st@naver.com>
2023-05-23 22:04:32 +0900

    feat : 데이터가 없을 경우 사용할 컴포넌트 제작

--- src/pages/Genre.js
+++ src/pages/Genre.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import tw from 'tailwind-styled-components';
-import { AiOutlineSearch } from 'react-icons/ai';
 import movieApi from '../apis/movieApi';
 import '../styles/color.css';
 import '../styles/font.css';
 import MovieCard from '../components/MovieCard';
 import Tag from '../components/Tag';
+import Empty from '../components/Empty';
 
 export default function Genre() {
   const MainContainer = tw.div`
@@ -21,19 +21,6 @@ export default function Genre() {
   `;
   const CardContainer = tw.div`
     flex flex-wrap
-  `;
-  const EmptyCardContainer = tw.div`
-    text-white 
-    px-8 my-8
-    w-full 
-    align-center
-  `;
-  const EmptyCard = tw.div`
-    text-3xl 
-    p-8 
-    flex flex-col 
-    items-center text-center 
-    animate-blink
   `;
 
   const [movieData, setMovieData] = useState(null);
@@ -52,6 +39,7 @@ export default function Genre() {
         setMovieData(json);
       });
   }, [tagList]);
+
   return (
     <MainContainer>
       <Tag tagList={tagList} setTag={setTag} />
@@ -69,15 +57,7 @@ export default function Genre() {
             return <MovieCard movieData={data} key={movieData.id} />;
           })
         ) : (
-          <EmptyCardContainer>
-            <EmptyCard>
-              <AiOutlineSearch size="50" />
-              <p className="mt-6">
-                movie not found <br />
-                search again
-              </p>
-            </EmptyCard>
-          </EmptyCardContainer>
+          <Empty />
         )}
       </CardContainer>
     </MainContainer>
