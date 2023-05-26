0f35f39c863d38d4a7e897dfb96ac501062cd491
novice-hero <highero700@gmail.com>
2023-05-26 19:53:40 +0900

    Comment: eslint 주석 제거

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable import/no-extraneous-dependencies */
 import React, { useState } from 'react';
 import tw from 'tailwind-styled-components';
 
